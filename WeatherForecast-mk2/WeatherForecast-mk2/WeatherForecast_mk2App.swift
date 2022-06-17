ceafe08b1fc2026313c709c8856d8b5ccb9387be
RomanKorsak <korsak-mc12@list.ru>
2022-06-17 11:15:58 +0300

    add another new proj

---
+++ WeatherForecast-mk2/WeatherForecast-mk2/WeatherForecast_mk2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecast_mk2App.swift
+//  WeatherForecast-mk2
+//
+//  Created by Роман Корсак on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecast_mk2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
